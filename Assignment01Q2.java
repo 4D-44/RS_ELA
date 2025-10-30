2328698d70330af67e502d2739e8fe24928f190d
Arthito Addoy Adhip <4dmensional@gmail.com>
2025-10-31 00:58:40 +0600

    huh

--- Assignment01Q2.java
+++ Assignment01Q2.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Assignment01Q2 {
+    public static int maxArea(ArrayList<Integer> height) {
+        int area = 0;
+        int l = 0, r = 0;
+
+        
+
+        return area;
+    }
     public static void main(String[] args) {
-    	
+        ArrayList<Integer> height = new ArrayList<>(Arrays.asList(1,8,6,2, 5,4,8,3,7));
+        ArrayList<Integer> height2 = new ArrayList<>(Arrays.asList(1,1));
+
+        System.out.println(maxArea(height));
+        System.out.println(maxArea(height2));
     }
 }
