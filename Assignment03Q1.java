d0b8c5f236adaeef0bb6967cc283d33cb776229a
Arthito Addoy Adhip <4dmensional@gmail.com>
2025-12-05 02:10:37 +0600

    e

--- Assignment03Q1.java
+++ Assignment03Q1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Assignment03Q1 {
     public static void main(String[] args) {
+        System.out.println("Enter the string: ");
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         Solution a = new Solution();
