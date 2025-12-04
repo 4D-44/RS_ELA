084532668f8bac14c98f9a0a0cd8a6b5e55791f6
Arthito Addoy Adhip <4dmensional@gmail.com>
2025-12-05 02:07:50 +0600

    e

---
+++ Assignment03Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Assignment03Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        Solution a = new Solution();
+        System.out.println(a.isValid(s));
+        System.out.println("cake");
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        boolean bool = false;
+        
+        return bool;
+    }
+}
