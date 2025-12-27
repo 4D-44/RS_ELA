4c780aafab992c02de7a05170bb713fa30f7fbd5
Arthito Addoy Adhip <4dmensional@gmail.com>
2025-12-27 10:56:27 +0600

    kichhui likhi ni ngl

---
+++ class6.java
@@ -0,0 +1,41 @@
+public class class6 {
+    // bubble sort
+    public void bubbleSort(int[] arr) {
+        // problem: height checker (1051 of leetcode)
+        // easy and efficient to do with bubble sort
+        /*
+    Students are asked to stand in non-decreasing order of heights for an annual photo.
+    Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+    Input: [1,1,4,2,1,3]
+    Output: 3
+    Explanation: 
+    Students with heights 4, 3 and the last 1 are not standing in the right positions.
+    Note:
+    1 <= heights.length <= 100
+    1 <= heights[i] <= 100
+        */
+
+    }
+
+    public void merge(int[] num1, int[] num2, int n) {
+        // problem: Merge sorted array (88 of leetcode)
+        /*
+        Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+        Note:
+        The number of elements initialized in nums1 and nums2 are m and n respectively.
+        You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+        Only store the new array in another array 
+        Example:
+        Input:
+        nums1 = [1,2,3,0,0,0], m = 3
+        nums2 = [2,5,6],       n = 3
+        Output: [1,2,2,3,5,6]
+        */
+
+    }
+
+    public static void main(String[] args) {
+        // try https://leetcode.ca/all/1502.html
+
+    }
+}
